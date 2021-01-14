bbb0be1a92c9202098ac6cab5bf0db27ddbc4d28
frogbubble711 <frogbubble711@gmail.com>
2021-01-14 01:19:25 -0400

    new one

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { readRemoteFile } from 'react-papaparse';
+import Graphic from './data/Graphic';
+import ScrollyTeller from './scrolly-teller';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './App.css';
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+const App = () => {
+  const [finishingStudy, setFinishingStudy] = useState(false);
+  const [girlOnFloor, setGirlOnFloor] = useState(false);
+  const [coworkers, setCoworkers] = useState(false);
+  const [treadmill, setTreadmill] = useState(false);
+
+  readRemoteFile('./finishing-studies.csv', {
+    header: true,
+    complete: data => {
+      !finishingStudy && finishingStudy !== data.data[0]['finishingStudies'] && setFinishingStudy(data.data[0]['finishingStudies']);
+    }
+  });
+
+  readRemoteFile('./girl-on-floor.csv', {
+    header: true,
+    complete: data => {
+      !girlOnFloor && girlOnFloor !== data.data[0]['girlOnFloor'] && setGirlOnFloor(data.data[0]['girlOnFloor']);
+    }
+  });
+
+  readRemoteFile('./coworkers.csv', {
+    header: true,
+    complete: data => {
+      !coworkers && coworkers !== data.data[0]['coworkers'] && setCoworkers(data.data[0]['coworkers']);
+    }
+  });
+
+  readRemoteFile('./treadmill.csv', {
+    header: true,
+    complete: data => {
+      !treadmill && treadmill !== data.data[0]['treadmill'] && setTreadmill(data.data[0]['treadmill']);
+    }
+  });
+
+  return (
+    <div className="container">
+      <Graphic panel={1}></Graphic>
+      <div data-aos="fade-up" className="finishingStudy">{finishingStudy}</div>
+      <Graphic panel={2}></Graphic>
+      <div data-aos="fade-down" className="girlOnFloor">{girlOnFloor}</div>
+      <Graphic panel={3}></Graphic>
+      <div data-aos="fade-right" className="coworkers">{coworkers}</div>
+      <Graphic panel={4}></Graphic>
+      <div data-aos="fade-left" className="treadmill">{treadmill}</div>
+    </div>
+  );
+};
+
+export default App;

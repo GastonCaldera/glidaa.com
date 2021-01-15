123a697cead7128979f30fbeed06920f12408092
ryann254 <keddelyronjoz@gmail.com>
2021-01-15 09:28:49 +0300

    Added intro section to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,31 @@
-import React, { useState } from 'react';
-import { readRemoteFile } from 'react-papaparse';
-import Graphic from './data/Graphic';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import './App.css';
-
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+import './scss/style.scss';
+import ScrollyTeller from './ScrollyTeller/ScrollyTeller';
+import intro from './Introduction/scrollyTellerConfig'
 
 
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+export default class App {
+  constructor() {
+    /** ScrollyTeller */
+    const storyConfiguration = {
+      /** The id of the <div> that will contain all of the page content */
+      appContainerId: 'app',
+      /** build an array of story sections
+       * Each section object should be a valid ScrollyTeller section configuration */
+      sectionList: [
+        intro
+      ],
+      /** optional function to receive the current sectionIdentifier,
+       * narrationIndex, narrationId, and narrationClass
+       * when narration blocks are entered */
+      onNarrationChangedFunction: () => {
 
-});
+      },
+    };
 
+    /** create the ScrollyTeller object to validate the config */
+    const storyInstance = new ScrollyTeller(storyConfiguration);
 
-const App = () => {
-  const [finishingStudy, setFinishingStudy] = useState(false);
-  const [girlOnFloor, setGirlOnFloor] = useState(false);
-  const [coworkers, setCoworkers] = useState(false);
-  const [treadmill, setTreadmill] = useState(false);
-
-  readRemoteFile('./finishing-studies.csv', {
-    header: true,
-    complete: data => {
-      !finishingStudy && finishingStudy !== data.data[0]['finishingStudies'] && setFinishingStudy(data.data[0]['finishingStudies']);
-    }
-  });
-
-  readRemoteFile('./girl-on-floor.csv', {
-    header: true,
-    complete: data => {
-      !girlOnFloor && girlOnFloor !== data.data[0]['girlOnFloor'] && setGirlOnFloor(data.data[0]['girlOnFloor']);
-    }
-  });
-
-  readRemoteFile('./coworkers.csv', {
-    header: true,
-    complete: data => {
-      !coworkers && coworkers !== data.data[0]['coworkers'] && setCoworkers(data.data[0]['coworkers']);
-    }
-  });
-
-  readRemoteFile('./treadmill.csv', {
-    header: true,
-    complete: data => {
-      !treadmill && treadmill !== data.data[0]['treadmill'] && setTreadmill(data.data[0]['treadmill']);
-    }
-  });
-
-  return (
-    <div className="container">
-      <Graphic panel={1}></Graphic>
-      <div data-aos="fade-up" className="finishingStudy">{finishingStudy}</div>
-      <Graphic panel={2}></Graphic>
-      <div data-aos="fade-down" className="girlOnFloor">{girlOnFloor}</div>
-      <Graphic panel={3}></Graphic>
-      <div data-aos="fade-right" className="coworkers">{coworkers}</div>
-      <Graphic panel={4}></Graphic>
-      <div data-aos="fade-left" className="treadmill">{treadmill}</div>
-    </div>
-  );
-};
-
-export default App;
+    /** parse data and build all HMTL */
+    storyInstance.render();
+  }
+}

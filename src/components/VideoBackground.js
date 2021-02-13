5c2df8dbe8c85768ddc1dba721c26d36a1fcf07d
Branyer Vergara <Branyervergarac@gmail.com>
2021-02-13 19:13:55 -0400

    moving gif to assets / images

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -4,12 +4,16 @@ import React from "react";
 import "../VideoBackground.css"
 
 const VideoBackground = React.memo(({src}) => {
+
+  const mobile = require(`../assets/images/${src}-mobile.gif`)
+  const pc = require(`../assets/images/${src}.gif`)
+
   return (
     <div style={{position:'absolute', top:'0', width:'98.8vw'}}>
     <picture>
-      <source srcSet={`${src}-mobile.gif`} media="(max-width: 650px)"></source>
-      <source srcSet={`${src}.gif`}></source>
-      <img  srcSet={`${src}.gif`} alt="videobackground" style={{width:'100%', height:'100vh'}}></img>
+      <source srcSet={mobile} media="(max-width: 650px)"></source>
+      <source srcSet={pc}></source>
+      <img  srcSet={pc} alt="videobackground" style={{width:'100%', height:'100vh'}}></img>
     </picture>
     </div>
   );
